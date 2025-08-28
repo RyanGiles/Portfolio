88a028aa01651ac68ebd0eb998464aae7f2998b2
Ryan Giles <rgiles0820@gmail.com>
2025-08-28 15:35:49 -0400

    video first try

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,10 @@ function Home() {
   return (
     <div className="homepage">
       <h1 className="homepage-title">HomePage</h1>
+      <video autoplay muted loop id="myVideo">
+        <source src="../../Assets/BG_Video.mp4" type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
     </div>
   );
 }
