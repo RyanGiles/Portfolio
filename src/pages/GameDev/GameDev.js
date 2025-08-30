2ca0bdcb1c4a9f4c0f16b36d2b5c2ff9dd0d413e
Ryan Giles <rgiles0820@gmail.com>
2025-08-30 18:01:12 -0400

    GameDev page functionality part 1

--- src/pages/GameDev/GameDev.js
+++ src/pages/GameDev/GameDev.js
@@ -1,19 +1,51 @@
 import "./GameDev.scss";
 import BG_Transition from "../../Assets/BG_Transition.mp4";
+import Projects from "./ProjectInfo.json";
+import { useState } from "react";
+
+
 
 function GameDev() {
+  const [images, setImages] = useState(Projects.images.home);
+  let home = true;
+
+  function getItem(index) {
+    if (home) {
+      let project;
+      switch (index){
+        case 0:
+          project = Projects.images.p1;
+          break;
+        case 1:
+          project = Projects.images.p2;
+          break;
+        case 2:
+          project = Projects.images.p3;
+          break;
+        case 3:
+          project = Projects.images.p4;
+          break;
+        default:
+          console.error("Switch case in getItems hit default");
+          break;
+      }
+      return <img src={images[index]} onClick={() => {setImages(project)}} alt="test" className="portfolio__img" />
+    } else {
+      //code here to find page index and then create html tags based on file type
+    }
+  }
+
   return (
     <div className="GameDev">
       <video autoPlay muted className="homepage-bg">
               <source src={BG_Transition} type="video/mp4" />
               Your browser does not support HTML5 video.
             </video>
-      <h1 className="GameDev-title">GameDev</h1>
       <div className="portfolio">
-        <div className="portfolio__item"></div>
-        <div className="portfolio__item"></div>
-        <div className="portfolio__item"></div>
-        <div className="portfolio__item"></div>
+        <div className="portfolio__item">{getItem(0)}</div>
+        <div className="portfolio__item">{getItem(1)}</div>
+        <div className="portfolio__item">{getItem(2)}</div>
+        <div className="portfolio__item">{getItem(3)}</div>
       </div>
     </div>
   );
