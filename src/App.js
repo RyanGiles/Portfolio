fc5c2a89d1156dcf77d89654b6a16e1259fcd3dd
Ryan Giles <rgiles0820@gmail.com>
2025-08-30 12:39:14 -0400

    Reworked lots of stuff

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
+import GameDev from "./pages/GameDev/GameDev";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="gameDev" element={<GameDev />} />
           <Route path="contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,15 +6,19 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="homepage">
+      <video autoPlay muted loop className="homepage-bg">
+        <source src={BG_Video} type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
       <header className="header">
         <div className="header__branding">
           <img src={siteLogo} alt="site logo" className="header__logo" />
-          <h1 className="header__title">Ryan Giles</h1>
+          <h1 className="header__title">Ryan<br/>Giles</h1>
         </div>
         <nav className="nav">
           <ul className="nav__list">
             <li className="nav__item">
-              <Link to="/">
+              <Link to="/gameDev">
                 <h3 className="nav__text">Play</h3>
               </Link>
             </li>
@@ -31,10 +35,6 @@ function Home() {
           </ul>
         </nav>
       </header>
-      <video autoPlay muted loop className="homepage-bg">
-        <source src={BG_Video} type="video/mp4" />
-        Your browser does not support HTML5 video.
-      </video>
     </div>
   );
 }
