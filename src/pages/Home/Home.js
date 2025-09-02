fe825c8f4593f93381c43f4272239bbc88d4ca66
Ryan Giles <rgiles0820@gmail.com>
2025-09-02 10:27:30 -0400

    adds underline and hover functionality to main menu

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -18,18 +18,33 @@ function Home() {
         <nav className="nav">
           <ul className="nav__list">
             <li className="nav__item">
-              <Link to="/gameDev">
-                <h3 className="nav__text">Play</h3>
+              <Link to="/gameDev" className="nav__link">
+                <h3 className="nav__text">
+                  <span className="nav__main">Play</span>
+                  <span className="nav__reveal">
+                    <span className="nav__subtext">—Game Development</span>
+                  </span>
+                </h3>
               </Link>
             </li>
             <li className="nav__item">
-              <Link to="/webDev">
-                <h3 className="nav__text">Controls</h3>
+              <Link to="/gameDev" className="nav__link">
+                <h3 className="nav__text">
+                  <span className="nav__main">Controls</span>
+                  <span className="nav__reveal">
+                    <span className="nav__subtext">—Web Development</span>
+                  </span>
+                </h3>
               </Link>
             </li>
             <li className="nav__item">
-              <Link to="/contact">
-                <h3 className="nav__text">Settings</h3>
+              <Link to="/gameDev" className="nav__link">
+                <h3 className="nav__text">
+                  <span className="nav__main">Settings</span>
+                  <span className="nav__reveal">
+                    <span className="nav__subtext">—Contact/About Me</span>
+                  </span>
+                </h3>
               </Link>
             </li>
           </ul>
