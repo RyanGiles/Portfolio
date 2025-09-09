232c173eba02c6b179da7304fd8ea9a608287091
Ryan Giles <rgiles0820@gmail.com>
2025-09-09 17:23:11 -0400

    set up text for home page

--- src/pages/GameDev/GameDev.js
+++ src/pages/GameDev/GameDev.js
@@ -4,17 +4,15 @@ import Projects from "./ProjectInfo.json";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-
 function GameDev() {
   const [images, setImages] = useState(Projects.images.home);
-  const [page, setPage] = useState('home');
+  const [page, setPage] = useState("home");
   const navigate = useNavigate();
 
   function getItem(index) {
-    if (page === 'home') {
+    if (page === "home") {
       let project;
-      switch (index){
+      switch (index) {
         case 0:
           project = Projects.images.p1;
           break;
@@ -31,47 +29,114 @@ function GameDev() {
           console.error("Switch case in getItems hit default");
           break;
       }
-      return <div className="portfolio__item"><img src={images[index]} onClick={() => {setImages(project); setPage(0)}} alt="test" className="portfolio__img" /></div>
+      if (images[index] !== undefined) {
+        if (
+          images[index].split(".")[1] === "png" ||
+          images[index].split(".")[1] === "jpg"
+        ) {
+          return (
+            <div className="portfolio__item">
+              <img
+                src={images[index]}
+                onClick={() => {
+                  setImages(project);
+                  setPage(0);
+                }}
+                alt="test"
+                className="portfolio__img"
+              />
+            </div>
+          );
+        } else {
+          return (
+            <div className="portfolio__item">
+              <p className="portfolio__text">{images[index]}</p>
+            </div>
+          );
+        }
+      }
     } else {
       let items = [];
       for (let i = 0; i < 4; i++) {
-        items.push(images[i+(page*4)]);
+        items.push(images[i + page * 4]);
       }
       if (items[index] === undefined) {
         return;
       }
-        if (items[index].split('.')[1] === "mp4") {
-          return (
+      if (items[index].split(".")[1] === "mp4") {
+        return (
           <div className="portfolio__item">
-          <video autoPlay controls loop src={items[index]} className="portfolio__img"></video>
-          </div>);
-        } else if ((items[index].split('.')[1] === "png") || (items[index].split('.')[1] === "jpg")) {
-          return (
-            <div className="portfolio__item">
-          <img src={items[index]} alt="projectimage" className="portfolio__img" />
-          </div>);
-        } else {
-          return <div className="portfolio__item"><p className="portfolio__text">{items[index]}</p></div>
-        }
+            <video
+              autoPlay
+              controls
+              loop
+              src={items[index]}
+              className="portfolio__img"
+            ></video>
+          </div>
+        );
+      } else if (
+        items[index].split(".")[1] === "png" ||
+        items[index].split(".")[1] === "jpg"
+      ) {
+        return (
+          <div className="portfolio__item">
+            <img
+              src={items[index]}
+              alt="projectimage"
+              className="portfolio__img"
+            />
+          </div>
+        );
+      } else {
+        return (
+          <div className="portfolio__item">
+            <p className="portfolio__text">{items[index]}</p>
+          </div>
+        );
+      }
     }
   }
 
   function renderPageButton(index) {
-    if (page !== 'home'){
-      if(index === 0 && page !== 0) {
-        return <img onClick={() => {setPage(page -1)}} src="/PrevPage.png" alt="back" className="page__back" />
-      } else if ((index === 1 && page < (Math.floor(images.length / 4) - 1)) || (index === 1 && page === (Math.floor(images.length/4) - 2) && images.length%4 === 0)) {
-        return <img onClick={() => {setPage(page +1)}} src="/NextPage.png" alt="back" className="page__next" />
+    if (page !== "home") {
+      if (index === 0 && page !== 0) {
+        return (
+          <img
+            onClick={() => {
+              setPage(page - 1);
+            }}
+            src="/PrevPage.png"
+            alt="back"
+            className="page__back"
+          />
+        );
+      } else if (
+        (index === 1 && page < Math.floor(images.length / 4) - 1) ||
+        (index === 1 &&
+          page === Math.floor(images.length / 4) - 2 &&
+          images.length % 4 === 0)
+      ) {
+        return (
+          <img
+            onClick={() => {
+              setPage(page + 1);
+            }}
+            src="/NextPage.png"
+            alt="back"
+            className="page__next"
+          />
+        );
       }
     }
   }
 
   function handleBack() {
-    if (page === 'home'){
+    if (page === "home") {
       navigate(-1);
-    } else{
+    } else {
       setImages(Projects.images.home);
-      setPage('home');
+      setPage("home");
     }
   }
 
@@ -83,7 +148,14 @@ function GameDev() {
       </video>
       {renderPageButton(0)}
       <div className="portfolio">
-        <img onClick={() => {handleBack()}} src="/back.png" alt="back" className="back" />
+        <img
+          onClick={() => {
+            handleBack();
+          }}
+          src="/back.png"
+          alt="back"
+          className="back"
+        />
         {getItem(0)}
         {getItem(1)}
         {getItem(2)}
